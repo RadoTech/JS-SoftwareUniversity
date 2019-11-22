848deab238a08f762dec969c258f8b65cee8d972
RTech <rado.radev81@gmail.com>
2019-11-22 18:08:05 +0200

    Adding Day of Week

---
+++ 4. Nested-Conditional-Statements/01. Day of Week.js
@@ -0,0 +1,25 @@
+function solve (input)
+{
+    let inputNumber = Number(input.shift());
+
+    switch (inputNumber) {
+        case 1: console.log(`Monday`);
+            break;
+        case 2: console.log(`Tuesday`);
+            break;
+        case 3: console.log(`Wednesday`);
+            break;
+        case 4: console.log(`Thursday`);
+            break;
+        case 5: console.log(`Friday`);
+            break;
+        case 6: console.log(`Saturday`);
+            break;
+        case 7: console.log(`Sunday`);
+            break;
+
+        default: console.log(`Error`)
+            break;
+    }
+}
+solve (["7"])
