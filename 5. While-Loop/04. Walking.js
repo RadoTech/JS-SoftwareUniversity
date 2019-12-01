c8a0e23dd5e580e7ff67d7fb42ae042ac6dad0c0
RTech <rado.radev81@gmail.com>
2019-12-01 12:48:02 +0200

    Adding Walking

---
+++ 5. While-Loop/04. Walking.js
@@ -0,0 +1,34 @@
+function solve(input)
+{
+    let command = input.shift();
+    let goal = 10000;
+    let stepsCounter = 0;
+
+    while (command != "Going home")
+    {
+        stepsCounter += Number(command);
+        if (stepsCounter >= goal)
+        {
+            console.log(`Goal reached! Good job!`);
+            return;
+        }
+        command = input.shift();
+        if (command == "Going home")
+        {
+            command = input.shift();
+            stepsCounter += Number(command);
+
+            if (stepsCounter >= goal)
+            {
+            console.log(`Goal reached! Good job!`);
+            return;
+            }
+            else
+            {
+                console.log(`${goal - stepsCounter} more steps to reach goal.`);
+                return;
+            }
+        }
+    }
+}
+solve(["1500", "300", "2500", "3000", "Going home", "200"])
