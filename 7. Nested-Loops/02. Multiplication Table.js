f896e5bc7a7d8e267530c3ca1f2e7c6b6f258cb4
RTech <rado.radev81@gmail.com>
2019-12-08 13:11:31 +0200

    Adding Multiplication Table(Nested-Loop Lab exercises)

---
+++ 7. Nested-Loops/02. Multiplication Table.js
@@ -0,0 +1,11 @@
+function solve()
+{
+for (let first = 1; first <= 10; first++)
+{
+    for (let second = 1; second <= 10; second++)
+    {
+        console.log(`${first} * ${second} = ${first * second}`)
+    }
+}
+}
+solve([])
